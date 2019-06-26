f0f0bef6df86b426a41a8e8c335b87748d0400d9
LadyKerr <kadiekerr@gmail.com>
2019-06-26 18:03:41 -0400

    added some dummydata styles

--- haircare/src/components/clientview/StylistList.js
+++ haircare/src/components/clientview/StylistList.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBContainer } from 'mdbreact';
+
 
 class StylistList extends React.Component {
   
   //remove testId and pass in actual id from api with no colon!!!
   pushToStylistPage = (id) => {
-    // let TestId=1
-    // console.log(id)
-      this.props.history.push(`/stylists/${id}`)
+    let TestId=3
+    console.log(id)
+      this.props.history.push(`/stylists/${TestId}`)
     }
 
   render() {
-    console.log(this.props);
+    //console.log(this.props);
     return(
       <div>
-          <h3>{this.props.stylist.username}</h3>
-          <p>{this.props.stylist.about}</p>
-          <p>Skills: {this.props.stylist.skills}</p>
-          <button onClick={this.pushToStylistPage}>View Profile</button>
+        <MDBContainer>
+          <MDBCard style={{ width: "22rem" }}>
+            <MDBCardImage className="img-fluid" src="https://source.unsplash.com/collection/391411" waves />
+            <MDBCardBody>
+              <MDBCardTitle>{this.props.stylist.username}</MDBCardTitle>
+              <MDBCardText>{this.props.stylist.about}</MDBCardText>
+              <MDBCardText>Top skills: {this.props.stylist.skills}</MDBCardText>
+              <MDBBtn color="amber" onClick={this.pushToStylistPage}>View Profile</MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBContainer>
+         <br/>
       </div>
     )
   }
--- haircare/src/components/clientview/StylistPage.js
+++ haircare/src/components/clientview/StylistPage.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import Loader from 'react-loader-spinner';
 import { withRouter } from 'react-router-dom';
 import { getStylistId } from '../../actions';
+import LightBox from './LightBox';
+import './stylist.css';
 
 class StylistPage extends React.Component {
   componentDidMount() {
@@ -17,17 +19,19 @@ class StylistPage extends React.Component {
         {this.props.fetchingStylists && ( 
             <Loader type="Puff" color="#ffb900" height="60" width="60" />
         )}
-        <h1>COMPONENT WORKS!! =) NEED ID OF STYLIST</h1>
-{                    <div> 
+        <h1><span>{this.props.stylistPerson.username}'s</span> P O R T F O L I O </h1>
+        
+                  { <div className="portfolio-container"> 
                       <img
-                        src={this.props.stylistPerson.profilePhoto}
+                        // src={this.props.stylistPerson.profilePhoto}
+                        src='https://source.unsplash.com/collection/391411'
                         alt={this.props.stylistPerson.name}
+                        className="portfolio-img"
                       />
-                      <p> {this.props.stylistPerson.username } </p>
-                      <p> {this.props.stylistPerson.about} </p>
-                      <p> {this.props.stylistPerson.skills} </p>
-                  </div>
-                }
+                      <p> {this.props.stylistPerson.about} </p> <br />
+                      <span> {this.props.stylistPerson.skills} </span>
+                  </div> }
+                <LightBox />
       </div>
     )
   }
